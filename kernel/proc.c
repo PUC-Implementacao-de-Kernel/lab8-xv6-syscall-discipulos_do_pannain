e6aa298f855668df88b15f51b4e035ad41597ae3
S0uza984 <diegohcsouza2004@gmail.com>
2026-05-06 15:38:24 -0300

    Implementa syscall trace no xv6

--- kernel/proc.c
+++ kernel/proc.c
@@ -168,6 +168,7 @@ freeproc(struct proc *p)
   p->chan = 0;
   p->killed = 0;
   p->xstate = 0;
+  p->trace_mask = 0;
   p->state = UNUSED;
 }
 
@@ -281,6 +282,8 @@ kfork(void)
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
+
+  np->trace_mask = p->trace_mask;
 
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
